49bb646254fae8539b1489c75e0bfd794816b196
Guilherme Quentel Melo <gqmelo@gmail.com>
2016-05-17 12:32:36 -0300

    Fix conda wrapper for Windows

--- exec_wrappers/templates/run-in_conda.bat
+++ exec_wrappers/templates/run-in_conda.bat
@@ -1,16 +1,20 @@
 @echo off
 
+@REM This script was based on the original conda activate.bat
+
+@setlocal
+
 @set "CONDA_DEFAULT_ENV=@CONDA_ENV_DIR@"
+@set PATH="%CONDA_DEFAULT_ENV%";"%CONDA_DEFAULT_ENV%\Scripts";"%CONDA_DEFAULT_ENV%\Library\bin";%PATH%
 
-@endlocal & (
-    @set PATH="%CONDA_DEFAULT_ENV%";"%CONDA_DEFAULT_ENV%\Scripts";"%CONDA_DEFAULT_ENV%\Library\bin";%PATH%
-
-    @REM Run any activate scripts
-    @if exist "%CONDA_DEFAULT_ENV%\etc\conda\activate.d" (
-        @pushd "%CONDA_DEFAULT_ENV%\etc\conda\activate.d"
-        @for %%g in (*.bat) do @call "%%g"
-        @popd
-    )
+@REM Run any activate scripts
+@if exist "%CONDA_DEFAULT_ENV%\etc\conda\activate.d" (
+    @pushd "%CONDA_DEFAULT_ENV%\etc\conda\activate.d"
+    @for %%g in (*.bat) do @call "%%g"
+    @popd
 )
 
+@REM Execute the given command
 %*
+
+@endlocal
