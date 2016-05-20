d12fe26a947b225671529fab49e89617daf75f9e
Guilherme Quentel Melo <gqmelo@gmail.com>
2016-05-20 13:53:03 -0300

    Small fixes to conda-wrappers output on Windows

--- conda_recipes/conda-wrappers/post-link.bat
+++ conda_recipes/conda-wrappers/post-link.bat
@@ -1,8 +1,8 @@
 
-if "%CONDA_DEFAULT_ENV%" == "" (
-    for /F %%i in ('conda info --root') do set "ENV_DIR=%%i"
-    @echo ""
-    @echo "CONDA_DEFAULT_ENV is not set. Assuming conda root env"
+@if "%CONDA_DEFAULT_ENV%" == "" (
+    @for /F %%i in ('conda info --root') do set "ENV_DIR=%%i"
+    @echo
+    @echo CONDA_DEFAULT_ENV is not set. Assuming conda root env
 ) else (
     @set "ENV_DIR=%CONDA_DEFAULT_ENV%"
 )
@@ -11,22 +11,22 @@ if "%CONDA_DEFAULT_ENV%" == "" (
 @set "SCRIPTS_DIR=%ENV_DIR%\Scripts"
 @set "WRAPPERS_DIR=%ENV_DIR%\Scripts\wrappers\conda"
 
-@echo "Creating wrappers from %BIN_DIR% to %WRAPPERS_DIR%"
-create-wrappers ^
+@echo Creating wrappers from %BIN_DIR% to %WRAPPERS_DIR%
+@create-wrappers ^
     -t conda ^
     -b %BIN_DIR% ^
     -d %WRAPPERS_DIR% ^
     --conda-env-dir %ENV_DIR%
 
-@echo "Creating wrappers from %SCRIPTS_DIR% to %WRAPPERS_DIR%"
-create-wrappers ^
+@echo Creating wrappers from %SCRIPTS_DIR% to %WRAPPERS_DIR%
+@create-wrappers ^
     -t conda ^
     -b %SCRIPTS_DIR% ^
     -d %WRAPPERS_DIR% ^
     --conda-env-dir %ENV_DIR%
 
-@echo "Creating wrappers from %ENV_DIR% to %WRAPPERS_DIR%"
-create-wrappers ^
+@echo Creating wrappers from %ENV_DIR% to %WRAPPERS_DIR%
+@create-wrappers ^
     -t conda ^
     -b %ENV_DIR% ^
     -d %WRAPPERS_DIR% ^
